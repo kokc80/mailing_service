850bf1106abf7c00b8c6e4b9cbd37fd38c30ea13
Kokc80 <kokc@inbox.ru>
2026-04-20 22:43:51 +0900

    попытка создать модель рассылки

--- mailing/urls.py
+++ mailing/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from mailing.apps import MailingConfig
-from mailing.views import RecipientListView, RecipientCreateView
-from mailing.views import HomeView
-from users.views import UsersListView, MailingListView
+from mailing.views import RecipientListView, RecipientCreateView, MessageListView
+from mailing.views import HomeView, MessageCreateView, MailingListView
+from users.views import UsersListView
 
 app_name = MailingConfig.name
 
@@ -11,4 +11,6 @@ urlpatterns = [
     path("recipient_list/", RecipientListView.as_view(), name="recipient_list"),
     path("recipient_create/",RecipientCreateView.as_view(), name="recipient_create"),
     path("mailings/all/", MailingListView.as_view(), name="mailings_list"),
+    path("messages/", MessageListView.as_view(), name="message_list"),
+    path("message/new/", MessageCreateView.as_view(), name="message_create"),
 ]
--- mailing/views.py
+++ mailing/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views import generic
-from django.views.generic import DetailView, ListView, TemplateView
-from mailing.models import Recipient, Message
+from django.views.generic import DetailView, ListView, TemplateView, CreateView
+from mailing.models import Recipient, Message, Mailing
 from mailing.forms import RecipientForm
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
@@ -45,19 +45,69 @@ class MessageListView(LoginRequiredMixin, ListView):
 
     def get_count(self):
         messages = self.get_queryset()
-        count = 0
-        for message in messages:
-            count += 1
-        return count
+        if messages is not None:
+            count = 0
+            for message in messages:
+                count += 1
+            return count
 
     def get_queryset(self):
-        queryset = cache.get("my_message_list")
-        if not queryset:
-            queryset = Message.objects.all()
+       # queryset = cache.get("my_message_list")
+       # if not queryset:
+        queryset = Message.objects.all()
             # cache.set("my_message_list", queryset, 60 * 15)
-        return queryset
+        # return queryset
+
+
+class MessageCreateView(LoginRequiredMixin, CreateView):
+    """Создание сообщения"""
+    model = Message
+    template_name = "mailing/message_form.html"
+    fields = ["subject", "body"]
+    success_url = reverse_lazy("mailing:message_list")
 
 
 class HomeView(generic.TemplateView):
     template_name = "home.html"
 
+
+class MailingListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    """Отображение списка рассылок"""
+
+    model = Mailing
+    template_name = "mailing/campaign_list.html"
+    context_object_name = "mailings"
+
+    def get_queryset(self):
+        if self.request.user.groups.filter(name="Менеджер").exists():
+            queryset = Mailing.objects.all()
+        else:
+            queryset = Mailing.objects.filter(owner=self.request.user)
+        return queryset
+
+    def get_active_status(self):
+        """Получение статуса возможности запуска рассылки"""
+        mailings = self.get_queryset()
+        active_status = True
+        for mailing in mailings:
+            active_status = mailing.status_active
+
+        return active_status
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["count"] = self.get_count()
+        context["is_manager"] = self.request.user.is_staff or self.request.user.groups.filter(name="Менеджер").exists()
+        context["is_status_active"] = self.get_active_status()
+        return context
+
+    def get_count(self):
+        """Получение количества рассылок"""
+        mailings = self.get_queryset()
+        if mailings is not None:
+            return mailings.count()
+        else:
+            return  0
+
+    def test_func(self):
+        return True
