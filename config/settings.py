e715d9464603cd50916c015c9db135a6a35094d0
Kokc80 <kokc@inbox.ru>
2026-05-12 13:20:46 +0900

    исправление рассылок 2026.05.12

--- config/settings.py
+++ config/settings.py
@@ -129,8 +129,8 @@ STATIC_URL = "mailing/static/"
 AUTH_USER_MODEL = "users.CustomUser"
 
 # Настройки для яндекс почты
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.yandex.ru"
 EMAIL_PORT = 465  # Исходящая почта, 992- Входящая почта
 EMAIL_USE_TLS = False
@@ -139,6 +139,6 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
-LOGIN_REDIRECT_URL = "mailing:campaign_list"
-LOGOUT_REDIRECT_URL = "mailing:campaign_list"
+LOGIN_REDIRECT_URL = "mailing:mailing_list"
+LOGOUT_REDIRECT_URL = "mailing:mailing_list"
 LOGIN_URL = "users:login"
